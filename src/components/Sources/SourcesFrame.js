dc75995ffde8be585b875a54b42760f4be758b35
Unknown <nizwa@skyshi.com>
2017-09-26 19:27:11 +0700

    change static tab view to scrolabble tab view

---
+++ src/components/Sources/SourcesFrame.js
@@ -0,0 +1,70 @@
+import React, { Component } from 'react'
+import { View, FlatList } from 'react-native'
+import { NavigationActions } from 'react-navigation'
+import { connect } from 'react-redux'
+import { applicationStyles } from '../../themes'
+
+import Loading from '../common/Loading'
+import SourceBox from './SourceBox'
+
+class SourcesFrame extends Component {
+  renderRow ({ item, index }) {
+    return (
+      <SourceBox
+        item={item}
+        index={index}
+        onPress={() => this.props.articlesScreen(item.id, item.name)}
+      />
+    )
+  }
+
+  renderLoading () {
+    if (this.props.loading) {
+      return (
+        <View style={{ marginTop: 8 }}>
+          <Loading />
+        </View>
+      )
+    }
+  }
+
+  renderSeparator () {
+    return <View style={{ height: 8 }} />
+  }
+
+  render () {
+    return (
+      <View style={applicationStyles.container}>
+        {this.renderLoading()}
+        <FlatList
+          ItemSeparatorComponent={() => this.renderSeparator()}
+          ListHeaderComponent={() => this.renderSeparator()}
+          ListFooterComponent={() => this.renderSeparator()}
+          showsVerticalScrollIndicator={false}
+          data={this.props.sources}
+          numColumns={2}
+          initialNumToRender={10}
+          keyExtractor={item => item.id}
+          renderItem={this.renderRow.bind(this)}
+          removeClippedSubviews={false}
+          contentContainerStyle={{ paddingHorizontal: 8 }}
+        />
+      </View>
+    )
+  }
+}
+
+function mapDispatchToProps (dispatch) {
+  return {
+    articlesScreen: (sourceId, sourceName) =>
+      dispatch(NavigationActions.navigate({
+        routeName: 'ListArticlesScreen',
+        params: { sourceId, sourceName }
+      }))
+  }
+}
+
+export default connect(
+  null,
+  mapDispatchToProps
+)(SourcesFrame)
--- src/components/Sources/index.js
+++ src/components/Sources/index.js
@@ -1,96 +1,58 @@
 import React, { Component } from 'react'
-import { View, FlatList } from 'react-native'
-import { NavigationActions } from 'react-navigation'
 import { connect } from 'react-redux'
 import { getSources } from '../../actions/sources'
-import { applicationStyles } from '../../themes'
-
-import Loading from '../common/Loading'
-import CategoriesBox from './CategoriesBox'
-import SourceBox from './SourceBox'
+import ScrollableTabView from 'react-native-scrollable-tab-view'
+import SourcesFrame from './SourcesFrame'
 
 class Sources extends Component {
   componentWillMount () {
     this.props.getSources('business')
-  }
-
-  renderRow ({ item, index }) {
-    return (
-      <SourceBox
-        item={item}
-        index={index}
-        onPress={() => this.props.articlesScreen(item.id, item.name)}
-      />
-    )
-  }
-
-  renderLoading () {
-    if (this.props.loading) {
-      return (
-        <View style={{ marginTop: 8 }}>
-          <Loading />
-        </View>
-      )
-    }
-  }
-
-  renderCategories () {
-    return (
-      <CategoriesBox
-        onBusinessPressed={() => this.props.getSources('business')}
-        onPoliticsPressed={() => this.props.getSources('politics')}
-        onTechnologyPressed={() => this.props.getSources('technology')}
-      />
-    )
-  }
-
-  renderSeparator () {
-    return <View style={{ height: 8 }} />
+    this.props.getSources('politics')
+    this.props.getSources('technology')
   }
 
   render () {
     return (
-      <View style={applicationStyles.container}>
-        {this.renderCategories()}
-        {this.renderLoading()}
-        <FlatList
-          ItemSeparatorComponent={() => this.renderSeparator()}
-          ListHeaderComponent={() => this.renderSeparator()}
-          ListFooterComponent={() => this.renderSeparator()}
-          showsVerticalScrollIndicator={false}
-          data={this.props.sources}
-          numColumns={2}
-          initialNumToRender={10}
-          keyExtractor={item => item.id}
-          renderItem={this.renderRow.bind(this)}
-          removeClippedSubviews={false}
-          contentContainerStyle={{ paddingHorizontal: 8 }}
+      <ScrollableTabView>
+        <SourcesFrame
+          tabLabel='Business'
+          sources={this.props.businessSources}
+          loading={this.props.businessLoading}
         />
-      </View>
+        <SourcesFrame
+          tabLabel='Politics'
+          sources={this.props.politicsSources}
+          loading={this.props.politicsLoading}
+        />
+        <SourcesFrame
+          tabLabel='Technology'
+          sources={this.props.technologySources}
+          loading={this.props.technologyLoading}
+        />
+      </ScrollableTabView>
     )
-  }
-}
-
-function mapStateToProps (state) {
-  return {
-    sources: state.sources.sources,
-    loading: state.sources.loading
-  }
-}
-
-function mapDispatchToProps (dispatch) {
-  return {
-    getSources: (category) => dispatch(getSources(category)),
-    articlesScreen: (sourceId, sourceName) =>
-      dispatch(NavigationActions.navigate({
-        routeName: 'ListArticlesScreen',
-        params: { sourceId, sourceName }
-      }))
   }
 }
 
 Sources.navigationOptions = {
   title: 'News App'
+}
+
+function mapStateToProps (state) {
+  return {
+    businessSources: state.businessSources.sources,
+    businessLoading: state.businessSources.loading,
+    politicsSources: state.politicsSources.sources,
+    politicsLoading: state.politicsSources.loading,
+    technologySources: state.technologySources.sources,
+    technologyLoading: state.technologySources.loading
+  }
+}
+
+function mapDispatchToProps (dispatch) {
+  return {
+    getSources: (category) => dispatch(getSources(category))
+  }
 }
 
 export default connect(
