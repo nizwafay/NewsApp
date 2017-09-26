db27e81fa46bce0fd6f1ae8f1148e95d7e3b5aed
Unknown <nizwa@skyshi.com>
2017-09-26 22:27:11 +0700

    add timeout

--- src/config/index.js
+++ src/config/index.js
@@ -1,4 +1,7 @@
 import { create } from 'apisauce'
 
-export const api = create({ baseURL: 'https://newsapi.org/v1' })
+export const api = create({
+  baseURL: 'https://newsapi.org/v1',
+  timeout: 10000
+})
 export const apiKey = 'd23fc7c660044940bd51b556d74a9fb0'
