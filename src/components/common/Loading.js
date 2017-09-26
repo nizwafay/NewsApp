7137d781fbb37be7a43ec29cd36afd6a49980452
Unknown <nizwa@skyshi.com>
2017-09-26 20:09:23 +0700

    restyle loading component

--- src/components/common/Loading.js
+++ src/components/common/Loading.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react'
 import { ActivityIndicator } from 'react-native'
+import { colors } from '../../themes'
 
 class Loading extends Component {
   render () {
     return (
       <ActivityIndicator
         size='large'
+        color={colors.primary.dark}
       />
     )
   }
